c405f593ab3952630370a02539b446a931ff5c85
tugameproject <tugameproject@gmail.com>
2017-11-17 00:46:10 +0900

    スクロールバーのテストなど

---
+++ Assets/#MYASSET/スクリプト/UI/ItemButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemButton : MonoBehaviour {
+
+    //[System.NonSerialized]
+    public GameObject item_prefab;
+
+    private bool isSelected;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(isSelected);
+	}
+
+    public void OnEvent()
+    {
+        Debug.Log("event");
+    }
+
+    public void IsSelected(bool b)
+    {
+        isSelected = b;
+    }
+}
