30b9a75e2452c41fba54504a1e89594ded77d0c6
yoksy-dot <c501506019@tokushima-u.ac.jp>
2017-11-28 01:06:27 +0900

    プレイヤークラス変数の実装　共通関数は思いつかなかった

--- Assets/#MYASSET/スクリプト/VR(プレイヤー)/PlayerSuper.cs
+++ Assets/#MYASSET/スクリプト/VR(プレイヤー)/PlayerSuper.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class PlayerSuper : MonoBehaviour {
 
-    
+    /*
+		プレイヤースーパークラスっぽいの 
+			サブクラス候補
+				持ち物管理
+				装備品
+				ステータス
+                UI?
+	*/
 
-	// Use this for initialization
-	void Start () {
-        
+
+    [SerializeField]
+    private GameObject SkillZone1,SkillZone2;//スキルクラスのを後程移植すること
+    [SerializeField]
+    private GameObject HipItemZone;//腰アイテム位置
+    [SerializeField]
+    private GameObject SwordRepwan;//抜剣位置
+
+    /*
+     * 位置参照はここで一括のほうがいいんじゃない？
+    public GameObject HandR, HandL;//手
+    public GameObject Eye;//頭の位置
+    */
+
+    // Use this for initialization
+    void Start () {
+		
 	}
 	
 	// Update is called once per frame
