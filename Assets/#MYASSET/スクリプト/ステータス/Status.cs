38ae09c8b2d8f88fffb8a2dd6965ac9b8420e3e8
tugameproject <tugameproject@gmail.com>
2017-12-15 23:37:20 +0900

    ステータスクラス

---
+++ Assets/#MYASSET/スクリプト/ステータス/Status.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Status : MonoBehaviour {
+    int hp;     //体力
+    int maxhp;  //最大体力
+    int mp;     //魔力
+    int maxmp;  //最大魔力
+    int atk;    //攻撃力
+    int def;    //防御力
+    int magicatk;//魔法攻撃力
+    int magicdef;//魔法防御力
+    float speed;  //移動速度
+    float flyspeed;//空中時の移動速度
+
+    public int HP
+    {
+      get { return hp; }
+    }
+    public int MAXHP
+    {
+        get { return maxhp; }
+    }
+    public int MP
+    {
+        get { return mp; }
+    }
+    public int MAXMP
+    {
+        get { return maxmp; }
+    }
+    public int ATK
+    {
+        get { return atk; }
+    }
+    public int DEF
+    {
+        get { return def; }
+    }
+    public int MAGICATK
+    {
+        get { return magicatk; }
+    }
+    public int MAGICDEF
+    {
+        get { return magicdef; }
+    }
+    public float SPEED
+    {
+        get { return speed; }
+    }
+    public float FLYSPEED
+    {
+        get { return flyspeed; }
+    }
+
+ 
+    public static Status operator +(Status a, Status b)
+    {
+        return new Status();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
