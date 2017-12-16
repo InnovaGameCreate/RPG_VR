2f54ae295c52da36c1d06db2b196ad801c6fbbf7
yoksy-dot <c501506019@tokushima-u.ac.jp>
2017-12-16 15:21:22 +0900

    ステータスクラス改良中

--- Assets/#MYASSET/スクリプト/ステータス管理/Status.cs
+++ Assets/#MYASSET/スクリプト/ステータス管理/Status.cs
@@ -3,7 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Status : MonoBehaviour {
+
+    int Lv;
+    int nowHP;
+
     Parameters baseparameters;
+
+    Buff GivingBuff;
+    Buff ReceiveBuff;
+
+    float WeaponProgress;//武器熟練度
+    float MageicProgress;//魔法熟練度
+
+    float NextExperiencePoint;//次のレベルまでの経験値量
+    float Experience;//現在経験値
+
+
+
     // Use this for initialization
     void Start () {
 		
