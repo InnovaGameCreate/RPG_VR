f938392d7246b861bed8796281421f2a708f8f49
MoriyamaHB <mhrbykm122@yahoo.co.jp>
2017-12-16 15:46:38 +0900

    まーじ

--- Assets/#MYASSET/スクリプト/人物/HumanBase.cs
+++ Assets/#MYASSET/スクリプト/人物/HumanBase.cs
@@ -8,18 +8,12 @@ public class HumanBase : MonoBehaviour
     Status humanstatus;         //ステータス
     bool is_fly;         //飛んでるか   
     BackPack bag;       //持ち物クラス
-<<<<<<< HEAD
-     //TODO   受バフリスト・与バフリスト　格納用　宣言
-=======
 
-    //与バフクラス・受バフクラス格納用リスト宣言
+    //受バフリスト・与バフリスト　格納用　宣言
     List<Buff> sendBuff = new List<Buff>();
     List<Buff> receiveBuff = new List<Buff>();
 
     //TODO   ダメージ計算クラス
-
->>>>>>> 03edea8ffc0aac187f9a34016c3b51606c5a226d
-
 
 
     private void Start()
