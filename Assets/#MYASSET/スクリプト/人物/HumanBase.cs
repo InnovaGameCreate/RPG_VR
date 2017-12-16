b70806ef4589eb407eb389932552f06a05b52538
miki kazuo <c501506022@tokushima-u.ac.jp>
2017-12-16 15:12:04 +0900

    体力魔力残量をパラメータクラスへ

--- Assets/#MYASSET/スクリプト/人物/HumanBase.cs
+++ Assets/#MYASSET/スクリプト/人物/HumanBase.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class HumanBase : MonoBehaviour {
     //人物スーパークラス
     Status humanstatus;         //ステータス
-    int hp;     //体力残量
-    int mp;     //魔力残量
     int lv = 1; //レベル
     int exp;    //現在の経験値
     const int expmax = 100;        //経験値最大量
@@ -14,14 +12,7 @@ public class HumanBase : MonoBehaviour {
     BackPack bag;       //持ち物クラス
      //TODO   ダメージ計算クラス
 
-    public int HP
-    {
-        get { return hp; }
-    }
-    public int MP
-    {
-        get { return mp; }
-    }
+
 
     private void Start()
     {
