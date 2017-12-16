b23fdf9044ccae464ffe57a79b19964a0fc996fd
miki kazuo <c501506022@tokushima-u.ac.jp>
2017-12-16 14:56:25 +0900

    人物スーパークラス

--- Assets/#MYASSET/スクリプト/ステータス管理/DamageCalculate.cs
+++ Assets/#MYASSET/スクリプト/ステータス管理/DamageCalculate.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DamageCalculate : MonoBehaviour {
-    int atk;
-    public static DamageCalculate operator +(Status status, Buff buff)
-    {
-        atk = status.ATK+buff.
-        return new DamageCalculate();
-    }
+
+
 }
--- Assets/#MYASSET/スクリプト/人物/HumanBase.cs
+++ Assets/#MYASSET/スクリプト/人物/HumanBase.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class HumanBase : MonoBehaviour {
     //人物スーパークラス
     Status humanstatus;         //ステータス
-    private int lv = 1; //レベル
-    private int exp;    //現在の経験値
-    private int expmax = 100;        //経験値最大量
-                                     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    int hp;     //体力残量
+    int mp;     //魔力残量
+    int lv = 1; //レベル
+    int exp;    //現在の経験値
+    const int expmax = 100;        //経験値最大量
+    bool is_fly;         //飛んでるか   
+    BackPack bag;       //持ち物クラス
+     //TODO   与バフクラス・受バフクラス格納用リスト宣言
+
+    public int HP
+    {
+        get { return hp; }
+    }
+    public int MP
+    {
+        get { return mp; }
+    }
+
+
 }
