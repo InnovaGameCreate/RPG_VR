9cdc2073094bfbe9640cbb088149c691fb4916e1
miki kazuo <c501506022@tokushima-u.ac.jp>
2017-12-28 13:12:38 +0900

    アセット追加

---
+++ Assets/#MYASSET/スクリプト/アイテム/Weapon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon :ItemBase
+{   //剣
+    [SerializeField]
+    private int endurance;  //耐久
+    protected override void Start()
+    {
+        ItemUse();
+        Debug.Log(parent.SendBuff);
+    }
+    public int Endurance
+    {
+        get { return endurance; }
+        set { endurance = value > 0 ? value : 0; }
+    }
+    public override bool ItemUse()
+    {
+        parent.SendBuff.Add(GetComponent<Buff>());
+        return true;
+    }
+        
+
+}
--- Assets/Tiny Fantasy Loot/Scripts/TinyThirdPersonUserControl.cs
+++ Assets/Tiny Fantasy Loot/Scripts/TinyThirdPersonUserControl.cs
@@ -1,6 +1,4 @@
-using System;
 using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
 
 namespace TinyDungeon
 {
