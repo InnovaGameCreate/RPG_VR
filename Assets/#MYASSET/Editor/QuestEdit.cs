b8f2bd41a8137b2381b6b8e95b070c824358d6fa
miki kazuo <c501506022@tokushima-u.ac.jp>
2018-02-10 17:58:38 +0900

    [クエスト]クエストスクリプト・Inspector改造・ゲームマネージャにクエスト用リスト作る予定

---
+++ Assets/#MYASSET/Editor/QuestEdit.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Quest))]
+public class QuestEdit : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        Quest obj = target as Quest;
+
+        obj.questType = (Quest.QuestType)EditorGUILayout.EnumPopup("QuestType", obj.questType);
+
+        //共通の注記 
+
+        EditorGUI.indentLevel++;    //インデントを入れる
+
+        switch (obj.questType)
+        {
+            case Quest.QuestType.FIGHT:
+                EditorGUILayout.HelpBox("討伐クエストの場合、対象モンスターの討伐数\n対象モンスター", MessageType.Info, true);
+                obj.ClearNum = EditorGUILayout.IntField("討伐数", obj.ClearNum);
+                obj.target = (GameObject)EditorGUILayout.ObjectField("討伐モンスター", obj.target, typeof(GameObject), false);
+                break;
+
+            case Quest.QuestType.COLLECT:
+                EditorGUILayout.HelpBox("採集クエストの場合、対象アイテムの採集数\n対象アイテム", MessageType.Info, true);
+                obj.ClearNum = EditorGUILayout.IntField("採集数", obj.ClearNum);
+                obj.target = (GameObject)EditorGUILayout.ObjectField("採集アイテム", obj.target, typeof(GameObject), false);
+
+                break;
+        }
+
+        EditorGUI.indentLevel--;    //インデントを戻す
+
+        //obj.iconImage = EditorGUILayout.TextField("Icon Image", obj.iconImage); //アイコン画像
+
+        EditorUtility.SetDirty(target);
+    }
+}
---
+++ Assets/#MYASSET/スクリプト/クエスト/Quest.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class Quest : MonoBehaviour
+{
+    [SerializeField, TooltipAttribute("クエストタイプ")]
+    public QuestType questType;
+
+    [SerializeField, TooltipAttribute("達成規定数：討伐数 or 採集数")]
+    public int ClearNum = 10;
+
+    public int achieveCount;      //達成数
+
+    private bool isClear;       //クリアしたかどうか
+
+    //ターゲット　　モンスター or アイテム
+    public GameObject target;
+
+    [SerializeField, TooltipAttribute("クエスト説明")]
+    [Multiline]
+    public string questText;//説明文
+
+    //クエストタイプ
+    public enum QuestType
+    {
+        FIGHT,          //討伐
+        COLLECT         //採集
+    }
+
+    public bool ISCLEAR
+    {
+        get { return isClear; }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.K))
+            checkTarget("ゴブリン");
+    }
+
+    //ターゲットなら達成数を増やす  
+    //TODO 敵を倒したとき　　アイテムを入手したときにこの関数を呼ぶ
+    void checkTarget(string tName)
+    {
+        // 手に入れたアイテム　or 倒した敵　がtargetと一致してるかどうか
+        var myRegExp = new Regex(target.name);
+        if (myRegExp.IsMatch(tName))
+            achieveCount++;
+        if (achieveCount >= ClearNum)
+            isClear = true;
+    }
+}
