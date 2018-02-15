c3296f95d1a56dbb7f7f329d86d428f78f93b4ac
yoksy-dot <c501506019@tokushima-u.ac.jp>
2018-02-16 02:34:12 +0900

    クラス作っただけ

---
+++ Assets/#MYASSET/スクリプト/ステータス管理/バフ/Buff_D_ATK.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buff_D_ATK : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
