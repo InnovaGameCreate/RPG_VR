c18308131df0e7ad49d0ca933195d49a91d8eab8
ishidashota <c611621050@tokushima-u.ac.jp>
2018-03-12 22:39:06 +0900

    声イベント途中までコミット

---
+++ Assets/#MYASSET/シーン/本編/VoiceEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class VoiceEvent : MonoBehaviour {
+
+    private AudioSource oberon;
+    private AudioSource villageDaugther;
+    
+
+    // Use this for initialization
+    void Start () {
+        oberon = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (IsFinished(oberon))
+        {
+            //シーンを民家に
+            SceneManager.LoadScene("民家");
+            
+        }
+
+	}
+
+    public bool IsFinished(AudioSource audio)
+    {
+        return audio.time == 0.0f && !audio.isPlaying;
+    }
+
+}
