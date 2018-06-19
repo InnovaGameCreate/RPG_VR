76b4712a27d5c7af49b83baa6af6c11f86773b83
DESKTOP-U4EQ6CT\梅川 俊樹 <mahouka.sao.kinsho315@gamil>
2018-06-19 17:15:01 +0900

    子供走る（アニメーションの改良点大あり）

---
+++ Assets/#MYASSET/スクリプト/人物/NPCAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent), typeof(Animator))]
+public class NPCAnimation : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Animator animator;
+
+    void Reset()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        animator.SetFloat("Speed", agent.velocity.sqrMagnitude);
+    }
+}
