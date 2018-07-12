cfdfca92156ce29c77137b81a44ec84aaf0cac6f
DESKTOP-U4EQ6CT\梅川 俊樹 <mahouka.sao.kinsho315@gamil>
2018-07-12 18:28:55 +0900

    追加

---
+++ Assets/#MYASSET/スクリプト/NPC関連/Aiku.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Aiku : TalkEvent
+{
+    //セリフ
+    enum TALK_NO
+    {
+        eSerif,//ランダムセリフ
+        eSerif2,
+
+        eTalkNum,
+    };
+
+    //イベントの種類
+    enum EVENT_NO
+    {
+        eNormal,
+        eDecline,
+        eEventNum,
+    }
+
+
+    bool[] talkFlag = new bool[(int)EVENT_NO.eEventNum];
+
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+        talkNum = (int)TALK_NO.eTalkNum;
+        //GetComponent<HumanNPC>().sendQuest();//クエスト受注
+
+    }
+
+    private bool NormalEventEnd()
+    {
+        return IsFinished(TalkAudioSource((int)TALK_NO.eSerif)) ||
+            IsFinished(TalkAudioSource((int)TALK_NO.eSerif2));
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        //普通の会話終了で選択画面描画
+        if (NormalEventEnd())
+        {
+            isTalk = false;
+            isPlaying = false;
+            PrintSelectUI();
+
+        }
+        if (IsTalk())
+            talkFlag[(int)EVENT_NO.eNormal] = true;
+
+
+
+        //通常会話であるならば
+        if (talkFlag[(int)EVENT_NO.eNormal])
+        {
+            //Debug.Log("tanasu12");
+            SetInputDevice(false);//リモコン操作停止
+            //ランダムで話す内容を変える
+            talkFlag[(int)EVENT_NO.eNormal] = false;
+            Talk(Random.Range((int)TALK_NO.eSerif, (int)TALK_NO.eSerif2 + 1));
+
+        }
+
+    }
+
+
+    public void ControllFlag()
+    {
+
+    }
+
+    public void SetNormalTalk()
+    {
+        SetInputDevice(false);//リモコン操作停止
+        talkFlag[(int)EVENT_NO.eNormal] = true;
+    }
+
+    public void SetDecline()
+    {
+        SetInputDevice(false);//リモコン操作停止
+        talkFlag[(int)EVENT_NO.eDecline] = true;
+
+
+    }
+
+    public void YesSelect()
+    {
+        GetComponent<HumanNPC>().sendQuest();//クエスト受注
+        YesNoUI.SetActive(false);
+        IsMove = true;
+    }
+}
---
+++ Assets/#MYASSET/スクリプト/NPC関連/Fatima.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fatima : TalkEvent
+{
+
+    //セリフ
+    enum TALK_NO
+    {
+        eSerif1,//いらっしゃい
+        eSerif2,//世間話
+        eTalkNum,
+    };
+
+
+    [SerializeField]
+    private GameObject buyUI;//購入用のUI（アタッチしてやる）
+
+    //セリフの数だけフラグを立てる
+    bool[] talkFlag = new bool[(int)TALK_NO.eTalkNum];
+
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+        base.talkNum = (int)TALK_NO.eTalkNum;
+
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        // base.Update();
+
+        Delta();
+        if (IsTalk() && !buyUI.activeSelf)
+            talkFlag[(int)TALK_NO.eSerif1] = true;
+        else
+            isTalk = false;
+
+
+        //通常会話であるならば
+        if (talkFlag[(int)TALK_NO.eSerif1])
+        {
+            //Debug.Log("tanasu12");
+            SetInputDevice(false);//リモコン操作停止
+            //ランダムで話す内容を変える
+            talkFlag[(int)TALK_NO.eSerif1] = false;
+            Talk((int)TALK_NO.eSerif1);
+
+        }
+
+        //通常会話終了でUIを描画
+        if (IsFinished(TalkAudioSource((int)TALK_NO.eSerif1)))
+        {
+            isTalk = false;
+            isPlaying = false;
+            SetUIObj(true);
+            PrintSelectUI();
+            Debug.Log("kiteru");
+        }
+
+
+
+
+        //セリフ２のフラグが立てば
+        if (talkFlag[(int)TALK_NO.eSerif2])
+        {
+            SetTalk(TALK_NO.eSerif2);
+        }
+
+        //セリフ2終了で
+        if (IsFinished(TalkAudioSource((int)TALK_NO.eSerif2)))
+        {
+            base.isTalk = false;
+            base.isPlaying = false;
+        }
+
+
+    }
+
+    private void SetTalk(TALK_NO no)
+    {
+        SetInputDevice(false);//リモコン操作停止
+        talkFlag[(int)no] = false;
+        Talk((int)no);
+    }
+
+    public void SetNormalTalk()
+    {
+        talkFlag[(int)TALK_NO.eSerif2] = true;
+    }
+
+    //購入を選択した場合
+    public void SelectBuy()
+    {
+        //選択のUIを消して
+        SetUIObj(false);
+        //購入画面を表示する
+        buyUI.SetActive(true);
+
+
+    }
+
+    //会話終了
+    public void EndTalk()
+    {
+        SetInputDevice(true);//リモコン操作停止
+        SetUIObj(false);
+        buyUI.SetActive(false);
+
+    }
+}
