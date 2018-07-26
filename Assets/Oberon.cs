577b591bec304cb6ea19d6739e8b4b42148fe54d
tugameproject <tugameproject@gmail.com>
2018-07-26 20:56:11 +0900

    動画取る前の確認（修正箇所あり）

---
+++ Assets/Oberon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oberon : MonoBehaviour {
+
+    private Vector3 pos;
+
+    void Start()
+    {
+
+        // MoveFloorオブジェクトの位置情報をposの中に代入する。
+        pos = transform.position;
+    }
+
+    void Update()
+    {
+
+        // （ポイント）
+        // Mathf.PingPong(float t, float length);
+        // tの値を0からlengthの範囲内で行ったりきたりさせる。
+        this.gameObject.transform.position = new Vector3(pos.x, pos.y + Mathf.PingPong(1, 0.2f), pos.z);
+    }
+}
